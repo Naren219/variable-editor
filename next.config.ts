d6c099ac68344e3962a8ecdeff8461a6cf42d53f
Naren219 <nmanikandan219@gmail.com>
2025-03-05 15:17:07 -0500

    js map

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.js\.map$/,
+      use: 'ignore-loader',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
